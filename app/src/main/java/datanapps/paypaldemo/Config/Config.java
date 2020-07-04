104cf08fd088b7be9a16ed295bc4571356230614
datanapps <datanapps@gmail.com>
2020-07-04 21:41:52 +0800

    update code

---
+++ app/src/main/java/datanapps/paypaldemo/Config/Config.java
@@ -0,0 +1,9 @@
+package datanapps.paypaldemo.Config;
+
+public class Config {
+
+
+    public static final String PAYPAL_CLIENT_ID = "ATRz6zV8z6GKgwzTLmx5_dEb0kSdwcPnIdLAhEs582d4aJUVoMN0V6-sTxbmTuzE0EXUPof5dl-T5bAE";
+
+    //public static final String PAYPAL_CLIENT_ID = "AW3FyAU_FbMwxVcu4wDdQcpUYEfxgwiRbZezuvccQjyHLm389OZvDTOGte10nSVmbXlpu8fxxWFiDqH4";
+}
